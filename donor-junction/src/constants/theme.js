1ae03b94e68b87518f24ab394e72588829798985
Srinithi <srinithiperiyasamy007@gmail.com>
2026-06-08 12:50:28 +0530

    setting and carousel

--- donor-junction/src/constants/theme.js
+++ donor-junction/src/constants/theme.js
@@ -11,8 +11,8 @@ export const COLORS = {
   INFO: '#0C447C'
 };
 
-const activeIp = '192.168.1.33';
+const activeIp = '192.168.1.56';
 
 export const API_URL = Platform.OS === 'web'
-  ? 'http://localhost/Donarjunction/backend-full'
-  : `http://${activeIp}/Donarjunction/backend-full`;
+  ? 'http://192.168.1.56/Donor-junction-app/backend-full'
+  : `http://${activeIp}/Donor-junction-app/backend-full`;
